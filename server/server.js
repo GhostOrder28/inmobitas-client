235228479c73b242e5c1c30d34ba4d48592b2e88
Ghost Order <kodaichi.kun@gmail.com>
2022-05-12 13:56:17 -0500

    static path change from build to public

--- server/server.js
+++ server/server.js
@@ -4,17 +4,17 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('build'));
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
 app.get('/*', (req, res) => {
   res.setHeader('content-type', 'application/javascript');
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
 
