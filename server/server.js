bd080ce3297c9f5fe5b89572e3e33e4d2dc82e2a
Ghost Order <kodaichi.kun@gmail.com>
2022-05-13 12:48:30 -0500

    server.js set copied from the react docs

--- server/server.js
+++ server/server.js
@@ -4,15 +4,20 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('build'));
+//app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-})
+//app.get('*', (req, res) => {
+  //res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+//})
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 //app.get('/', (req, res) => {
   //res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
