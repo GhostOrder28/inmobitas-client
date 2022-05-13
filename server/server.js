16ad387c3e4407d587c8bf1fb149d9d1fafbbd85
Ghost Order <kodaichi.kun@gmail.com>
2022-05-13 12:44:25 -0500

    last commit reverted

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static('build'));
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
