c77bddae8d00424d7800f7ca39af3274252d2c2a
Ghost Order <kodaichi.kun@gmail.com>
2022-05-14 11:29:44 -0500

    fixed path route handler

--- server/server.js
+++ server/server.js
@@ -4,26 +4,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-//app.use(express.static('build'));
 app.use(express.static(path.join(__dirname, '../build/')));
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
 
-//app.get('*', (req, res) => {
-  //res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-//})
-
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
-
-//app.get('/', (req, res) => {
-  //res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-//})
-//app.get('/*', (req, res) => {
-  //res.setHeader('content-type', 'application/javascript');
-  //res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-//})
-
