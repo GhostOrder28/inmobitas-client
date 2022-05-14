843c1bc6a8ba6d651d41e3094d419a56b2340481
Ghost Order <kodaichi.kun@gmail.com>
2022-05-14 16:31:43 -0500

    logotype displayed in the navbar was improved with the correct font, compression lib added to server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const enforce = require('express-sslify');
+const compression = require('compression');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ app.use(express.static(path.join(__dirname, '../build/')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(enforce.HTTPS({ trustProtoHeader: true }))
+  app.use(compression);
   app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '../build/index.html'));
   });
