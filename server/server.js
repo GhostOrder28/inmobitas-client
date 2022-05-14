f46b576acba35d4f80d0c7d6209bd0f47293f7a6
Ghost Order <kodaichi.kun@gmail.com>
2022-05-14 12:50:48 -0500

    enforcer added to server.js and react start script was passed into a new script in package.json

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const path = require('path');
+const enforce = require('express-sslify');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../build/')));
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }))
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+  });
+}
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+app.listen(port, err => {
+  if (err) throw err;
+  console.log(`Server is up on port ${port}`);
 });
